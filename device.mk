e3f792e8b1f3352be09c8efa06c13a9494d58cd2
Christopher <mizdrake7@gmail.com>
2022-10-30 07:20:25 +0530

    X01AD: Let's Ship Lineage Aperture Camera
    * Taken from here https://github.com/SebaUbuntu/android_packages_apps_Aperture
    
    Change-Id: I85c29e4c63ab1b70f4e12f25d00b58186c719f04
    Signed-off-by: Christopher <mizdrake7@gmail.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>
    Signed-off-by: yanzszs <yanzszs46@gmail.com>

--- device.mk
+++ device.mk
@@ -177,7 +177,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    libstdc++.vendor
+    libstdc++.vendor \
+    ApertureCam
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
